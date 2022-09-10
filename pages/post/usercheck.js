fd930e488e5655122f50331e1525ca3d6063518a
Emile2020 <emiledebruyckere@gmail.com>
2022-09-10 17:29:49 +0200

    Added headers to all files

--- pages/post/usercheck.js
+++ pages/post/usercheck.js
@@ -1,14 +1,18 @@
 const { read } = require('fs');
 
 //start the module.exports function
-module.exports = (app, fs, json, nodemailer, cryptojs, transporter, hash_token, token) => {
+module.exports = (app, fs, json, nodemailer, cryptojs, transporter) => {
     //create a post request
     app.post('/usercheck', (req, res) => {
+        //set the 'allow-control-allow-origin' header for replit
+        res.header("Access-Control-Allow-Origin", "*");
+        //fix 'Blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource.' error
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         //check if token is valid
-        if (req.body.token === token) {
+        if (req.body.token === 'token') {
             //get json data
             var json_data = require('../../database.json')
-            var hash = cryptojs.DES.decrypt(json_data.user[req.body.mail].password, hash_token).toString(cryptojs.enc.Utf8);
+            var hash = cryptojs.DES.decrypt(json_data.user[req.body.mail].password, 'sleepyamr is sleepy').toString(cryptojs.enc.Utf8);
             if (json_data.user[req.body.mail].password = req.body.password) {
                 //send success
                 var mailOptions = {
